69ad280f06c15d5bcf20e33a0afa58998941e860
DALyskov <DALyskov@gmail.com>
2019-10-26 23:19:34 +0300

    Задание 8.3 - остались стили и кнопка сброс

--- js/add-img.js
+++ js/add-img.js
@@ -3,15 +3,13 @@
 (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
-  var fileChooser = document.querySelector('.ad-form-header__input');
-  var adFormPreview = document.querySelector('.ad-form-header__preview img');
-  var dropAreaHeader = document.querySelector('.ad-form-header__drop-zone');
-  var dropArea = document.querySelector('.ad-form__drop-zone');
-  var adFormFoto = document.querySelector('.ad-form__photo');
-
+  var adFormFileChooser = window.page.adForm.querySelector('.ad-form-header__input');
+  var adFormPreview = window.page.adForm.querySelector('.ad-form-header__preview img');
+  var dropAreaHeader = window.page.adForm.querySelector('.ad-form-header__drop-zone');
+  var dropArea = window.page.adForm.querySelector('.ad-form__drop-zone');
+  var adFormFoto = window.page.adForm.querySelector('.ad-form__photo');
   var adFormFileChoosers = window.page.adForm.querySelectorAll('input[type="file"]');
-  var adFormFiledropAreas = [dropAreaHeader, dropArea];
-
+  var adFormFileDropAreas = [dropAreaHeader, dropArea];
 
   function displayImg(file) {
     var fileName = file.name.toLowerCase();
@@ -27,53 +25,45 @@
     }
   }
 
-  function onInputImgChange() {
-    var files = Array.prototype.slice.call(this.files);
-
-    if (this !== fileChooser) {
-      // var fileName = file.name.toLowerCase();
-      // var matches = FILE_TYPES.some(function (v) {
-      //   return fileName.endsWith(v);
-      // });
-
-      // if (matches) {
-      //   var template = adFormPreview;
-      //   var elm = template.cloneNode(true);
-      //   elm.alt = 'Фотография жилья';
-
-      //   var reader = new FileReader();
-      //   reader.addEventListener('load', function () {
-      //     elm.src = reader.result;
-      //   });
-      //   reader.readAsDataURL(file);
-
-      //   adFormFoto.append(elm);
-      // }
-      console.dir(file);
-      files.forEach(function (v) {
-        var fileName = v.name.toLowerCase();
-        var matches = FILE_TYPES.some(function (it) {
-          return fileName.endsWith(it);
-        });
-
-        if (matches) {
-          var template = adFormPreview;
-          var elm = template.cloneNode(true);
-          elm.alt = 'Фотография жилья';
-
-          var reader = new FileReader();
-          reader.addEventListener('load', function () {
-            elm.src = reader.result;
-          });
-          reader.readAsDataURL(v);
-
-          adFormFoto.append(elm);
-        }
+  function displayImgs(files) {
+    files.forEach(function (v) {
+      var fileName = v.name.toLowerCase();
+      var matches = FILE_TYPES.some(function (it) {
+        return fileName.endsWith(it);
       });
+      if (matches) {
+        var template = adFormPreview;
+        var elm = template.cloneNode(true);
+        elm.alt = 'Фотография жилья';
+
+        var reader = new FileReader();
+        reader.addEventListener('load', function () {
+          elm.src = reader.result;
+        });
+        reader.readAsDataURL(v);
+        adFormFoto.append(elm);
+      }
+    });
+  }
+
+  function checkInput(elm, standardElm, data) {
+    if (elm !== standardElm) {
+      displayImgs(data);
     } else {
-      var file = files[0];
+      var file = data[0];
       displayImg(file);
     }
+  }
+
+  function onInputImgChange() {
+    var files = Array.prototype.slice.call(this.files);
+    checkInput(this, adFormFileChooser, files);
+  }
+
+  function onAreaDrop(evt) {
+    var data = evt.dataTransfer;
+    var files = Array.prototype.slice.call(data.files);
+    checkInput(this, dropAreaHeader, files);
   }
 
   adFormFileChoosers.forEach(function (v) {
@@ -92,37 +82,7 @@
     });
   });
 
-  function onAreaDrop(evt) {
-    var data = evt.dataTransfer;
-    var files = Array.prototype.slice.call(data.files);
-    if (this !== dropAreaHeader) {
-      files.forEach(function (v) {
-        var fileName = v.name.toLowerCase();
-        var matches = FILE_TYPES.some(function (it) {
-          return fileName.endsWith(it);
-        });
-
-        if (matches) {
-          var template = adFormPreview;
-          var elm = template.cloneNode(true);
-          elm.alt = 'Фотография жилья';
-
-          var reader = new FileReader();
-          reader.addEventListener('load', function () {
-            elm.src = reader.result;
-          });
-          reader.readAsDataURL(v);
-
-          adFormFoto.append(elm);
-        }
-      });
-    } else {
-      var file = files[0];
-      displayImg(file);
-    }
-  }
-
-  adFormFiledropAreas.forEach(function (v) {
+  adFormFileDropAreas.forEach(function (v) {
     v.addEventListener('drop', onAreaDrop);
   });
 })();
